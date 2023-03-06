50ad86530e61285640bee4bdb50e16b1cb6360d4
Регина <rmashkovtseva@gmail.com>
2023-03-06 20:53:38 +0300

    Подготовка

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,27 @@
 package com.company;
 
 public class Main {
+    public static Movie[] getMovies() {
+        return new Movie[]{
+                new Movie("Начало", 2010, 16),
+                new Movie("Конец", 2011, 10),
+                new Movie("Середина", 2012, 18),
 
+
+        };
+    }
+
+    public static Theatre[] getTheatres() {
+        return new Theatre[]{
+                new Theatre("Анна Каренина", 2017, 16),
+                new Theatre("Анна Иванова", 2018, 10),
+                new Theatre("Анна Сидорова", 2019, 18),
+
+        };
+    }
+    public static void validEvent(Event event){
+//        if (event.title.equals("") || event.title == null || event.age == null)
+    }
     public static void main(String[] args) {
 	// write your code here
     }
